1d7a13d5613e956b37c4a9661806088af39f01c0
sunshj <sunshj317@qq.com>
2024-10-23 21:19:54 +0800

    fix: deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
     '@element-plus/nuxt',
     '@nuxt/devtools',
     '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
     'nuxt-auth-utils'
   ],
 
