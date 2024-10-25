e22acba824a4f4db49c951a86a89e9aabbe931ae
sunshj <sunshj317@qq.com>
2024-10-25 17:59:13 +0800

    chore: remove migrations as it is still in the early stages

--- prisma/migrations/20241024094311_user_role/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `comments_userId_fkey` ON `comments`;
-
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER';
-
--- AddForeignKey
-ALTER TABLE `comments` ADD CONSTRAINT `comments_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
