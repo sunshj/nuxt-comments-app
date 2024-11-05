33e17ae34a1f9d3b6d1951d12ddf8f54ba4fa3cb
sunshj <sunshj317@qq.com>
2024-11-05 13:52:47 +0800

    feat(unocss): add justify-safe-center rule and update flex-center shortcut

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,8 @@ import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [presetUno({ important: '#__nuxt' })],
+  rules: [['justify-safe-center', { 'justify-content': 'safe center' }]],
   shortcuts: {
-    'flex-center': 'flex justify-center items-center'
+    'flex-center': 'flex justify-safe-center items-center'
   }
 })
